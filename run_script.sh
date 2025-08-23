ce6b622e33ebe5dc54eec0d44b9a87320388206f
Brian Dooley <brian.dooley9@gmail.com>
2025-08-23 16:33:20 +0000

    feat: update run_script.sh to accept argument links
    The script now takes a YouTube link as the first arguement
    
    Signed-off-by: Brian Dooley <brian.dooley9@gmail.com>

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+# Wrapper to run the YouTube Transcript Generator
+# Usage: ./run_script.sh <youtube_url> [additional_flags]
 
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <youtube_url> [options]"
+  echo "Example: $0 https://youtube.com/watch?v=abcd1234 --summarize"
+  exit 1
+fi
+
+YOUTUBE_URL=$1
+shift  # remove first arg so any extra flags get passed through
+
+# Activate virtual environment if you use one
 source ~/myenv/bin/activate
+# source venv/bin/activate
 
-python3 main.py
-
+python3 main.py "$YOUTUBE_URL" "$@"
 
